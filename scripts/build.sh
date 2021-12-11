f7c3a7abc8eb0929ee4848fb6c7913c138a30227
Paul Barker <paul.barker@sancloud.com>
2021-12-11 15:23:01 +0000

    Add build script
    Signed-off-by: Paul Barker <paul.barker@sancloud.com>

---
+++ scripts/build.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# Copyright (C) 2021 SanCloud Ltd
+# SPDX-License-Identifier: Apache-2.0
+
+set -euo pipefail
+
+if [[ -z "$1" ]]; then
+    echo "Usage: $0 IMAGENAME"
+    exit 1
+fi
+
+if [[ ! -d "$1" ]]; then
+    echo "Invalid image name: $1"
+    exit 1
+fi
+
+cd $1
+podman build -t ghcr.io/sancloudltd/$1 .
